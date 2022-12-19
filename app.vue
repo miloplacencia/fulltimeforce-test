aa5cf37e52703140a531aea3db3aa641ca097adf
miloplacencia <placenciameister@gmail.com>
2022-12-19 03:47:30 +0000

    Added base body styles

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: { lang: "es-CL" },
+  title: "Fulltime Force Test | Camilo Placencia",
+});
+</script>
+<style lang="scss">
+body {
+  height: 100vh;
+  @apply bg-gray-100 text-gray-800 text-xl p-8;
+}
+</style>
